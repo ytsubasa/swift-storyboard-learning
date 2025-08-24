f53c886114e2e5d11e3eb0ca4732f430b9c36f30
Y.Tsubasa <125607203+ytsubasa@users.noreply.github.com>
2025-08-24 13:42:05 +0900

    しっかりと制約を考えてする

---
+++ swift-storyboard-learning/swift-storyboard-learning/NextViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewViewController.swift
+//  swift-storyboard-learning
+//
+//  Created by 吉田翔 on 2025/08/24.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.red // 背景色
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- swift-storyboard-learning/swift-storyboard-learning/ViewController.swift
+++ swift-storyboard-learning/swift-storyboard-learning/ViewController.swift
@@ -9,17 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
- 
+    @IBOutlet weak var button: UIButton!
+    
     
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        button.setTitle("クリック", for: UIControl.State.normal) // 追加
         
         imageView.image = UIImage(named: "curry")
+        
+        self.view.backgroundColor = .white
     }
 
-
+    @IBAction func buttonAction(_ sender: Any) {
+        
+        let nextViewController = self.storyboard?.instantiateViewController(withIdentifier: "NextView") as! NextViewController
+//                nextViewController.modalPresentationStyle = .fullScreen
+                self.present(nextViewController, animated: true, completion: nil)
+    }
+    
 }
 
